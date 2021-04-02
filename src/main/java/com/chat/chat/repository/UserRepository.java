6213ed38a9c5fa71aedc3c285746de26e3044c66
Beksultan Tatenov <bekatatenov@gmail.com>
2021-04-02 14:54:53 +0600

    add empty repositories for each model

---
+++ src/main/java/com/chat/chat/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.chat.chat.repository;
+
+import com.chat.chat.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
